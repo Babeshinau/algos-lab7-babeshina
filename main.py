833045a170c9ff167d134a703afae633d2c0ee77
Babeshinau <swcfes@gmail.com>
2024-12-24 01:11:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
-"""
-Задание №1
+#Задание1
+def is_palindrome(word):
+    word = word.lower()
+    word = ''.join(char for char in word if char.isalnum())
+    return word == word[::-1]
 
-Задание: Написать программу, которая принимает на вход слово и проверяет, является ли оно палиндромом.
-Условия:
+user_input = input("Введите слово: ")
 
-    Программа должна быть нечувствительна к регистру.
-    Игнорировать пробелы и знаки пунктуации.
-Пример использования:
-    isPalindrom("level") # True
-    isPalindrom("hello") # False
+if is_palindrome(user_input):
+    print(f"Слово '{user_input}' является палиндромом.")
+else:
+    print(f"Слово '{user_input}' не является палиндромом.")
+    
+#Задание2
+def is_palindrom(word):
+    word = word.lower()
+    word = ''.join(char for char in word if char.isalnum())
+    return word == word[::-1]
 
-Задание №2
+def find_palindromes(words):
+    palindromes = [word for word in words if is_palindrom(word)]
+    return palindromes
 
-Задание: Написать программу, которая принимает список слов и проверяет, какие из них являются палиндромами.
-Условия:
+user_input = input("Введите слова через пробел: ")
+words_list = user_input.split()
 
-    Слова передаются в виде списка через ввод пользователя.
-    Программа должна вывести все палиндромы из списка.
+palindromes = find_palindromes(words_list)
 
-Пример использования:
-    isPalindromList(["hello", "list", "level"]) # ["level"]
+if palindromes:
+    print("Палиндромы из списка:", palindromes)
+else:
+    print("Палиндромов нет.")
 
-Задание №3
+#Задание3
+import string
 
-Задание: Написать программу, которая ищет все палиндромы в строке текста.
-Условия:
+def is_palindrom(word):
+    word = word.lower()
+    word = ''.join(char for char in word if char.isalnum())
+    return word == word[::-1]
 
-    Программа должна игнорировать регистр и знаки пунктуации.
-    Если палиндромы повторяются, выводить их только один раз.
+def find_palindromes_in_text(text):
+    text = text.lower()
+    text = ''.join(char if char.isalnum() or char.isspace() else ' ' for char in text)
+    words = text.split()
+   
+    palindromes = set(word for word in words if is_palindrom(word))
+    return palindromes
 
-Пример использования isPalindromString("Madam, Anna went to the civic center") # ["madam", "anna", "civic"]
-"""
+user_input = input("Введите текст: ")
+
+palindromes = find_palindromes_in_text(user_input)
+
+if palindromes:
+    print("Найденные палиндромы:", ', '.join(palindromes))
+else:
+    print("Палиндромов нет.")
